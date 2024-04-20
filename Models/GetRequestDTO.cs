da2b3f07e3899ebeceed72bb63e14b6d73f2716a
Robu Corina <corinaflorentina28@gmail.com>
2024-04-20 10:56:01 +0300

    Add request

---
+++ Models/GetRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace healthy_lifestyle_web_app.Models
+{
+    public class GetRequestDTO
+    {
+        public int Id { get; set; }
+        public int FoodId { get; set; }
+        public GetFoodAdminDTO? GetFoodAdmin { get; set; }
+    }
+}
