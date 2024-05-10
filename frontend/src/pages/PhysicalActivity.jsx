566eca1b51f8790142d7b52e6195e2db2483852c
Alexandra <115414921+AlexandraUc@users.noreply.github.com>
2024-05-10 18:26:37 +0300

    Revert "Added physical activity"
    This reverts commit 557f70f455d2f2ce3a453708a4ed4d1431386f55.

--- frontend/src/pages/PhysicalActivity.jsx
+++ frontend/src/pages/PhysicalActivity.jsx
@@ -1,58 +1,6 @@
 import React from "react";
 
 function PhysicalActivity() {
-    const [physicalActivities, setPhysicalActivities] = useState([]);
-    const [allError, setAllError] = useState(null);
-
-    const [activityName, setActivityName] = useState('');
-    const [activity, setActivity] = useState(null);
-    const [nameError, setNameError] = useState(null);
-
-    const [muscleName, setMuscleName] = useState('');
-    const [activitiesByMuscle, setActivitiesByMuscle] = useState([]);
-    const [muscleError, setMuscleError] = useState(null);
-
-    const getAllActivites = async () => {
-        setAllError(null);
-
-        try {
-            const response = await axios.get('https://localhost:7094/api/PhysicalActivities');
-            setPhysicalActivities(response.data);
-        } catch (error) {
-            setPhysicalActivities([]);
-            setAllError('No activites found');
-            console.log(error);
-        }
-    };
-
-    const getByName = async () => {
-        setNameError(null);
-
-        try {
-            const response = await axios.get(`https://localhost:7094/api/PhysicalActivities/${activityName}`);
-
-            setActivity(response.data);
-        } catch (error) {
-            setActivity(null);
-            setNameError('No activity with this name');
-            console.log(error);
-        }
-    };
-
-    const getByMuscle = async () => {
-        setMuscleError(null);
-
-        try {
-            const response = await axios.get(`https://localhost:7094/api/PhysicalActivities/target-${muscleName}`);
-
-            setActivitiesByMuscle(response.data);
-        } catch (error) {
-            setActivitiesByMuscle([]);
-            setMuscleError('No activities targeting this muscle');
-            console.log(error);
-        }
-    };
-
     return (
         <div>
             <p>PhysicalActivity page</p>
