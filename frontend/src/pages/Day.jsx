05b9de595346c2d29f035569373bf6455cf25068
ana <anamaria.hodivoianu@gmail.com>
2024-05-18 13:55:53 +0300

    added change grams and minutes frontend

--- frontend/src/pages/Day.jsx
+++ frontend/src/pages/Day.jsx
@@ -8,6 +8,10 @@ function Day() {
     const navigate = useNavigate();
     const [completeDay, setCompleteDay] = useState(null);
     const [date, setDate] = useState(new Date());
+    const [grams, setGrams] = useState({});
+    const [updateGramsError, setUpdateGramsError] = useState(null);
+    const [minutes, setMinutes] = useState({});
+    const [updateMinutesError, setUpdateMinutesError] = useState(null);
 
     const formatDate = (date) => {
         const year = date.getFullYear();
@@ -28,15 +32,16 @@ function Day() {
             });
             return response.data;
         } 
-        catch(error) {
+        catch (error) {
             console.log(error);
+            throw error;
         }
     };
 
     const getCompleteDay = async (date) => {
         try {
             const day = await getDay(date);
-        
+
             const dayFoodsPromises = day.dayFoods.map(async (food) => {
                 const response = await axios.get(`https://localhost:7094/api/Food/by-id/${food.foodId}`, {
                     headers: {
@@ -45,7 +50,7 @@ function Day() {
                 });
                 return { ...food, details: response.data };
             });
-        
+
             const dayActivitiesPromises = day.dayPhysicalActivities.map(async (activity) => {
                 const response = await axios.get(`https://localhost:7094/api/PhysicalActivities/by-id/${activity.physicalActivityId}`, {
                     headers: {
@@ -54,14 +59,69 @@ function Day() {
                 });
                 return { ...activity, details: response.data };
             });
-        
+
             const dayFoods = await Promise.all(dayFoodsPromises);
             const dayActivities = await Promise.all(dayActivitiesPromises);
-        
-            setCompleteDay({ ...day, dayFoods, dayPhysicalActivities: dayActivities });
-        }
-        catch(error) {
+
+            const completeDayData = { ...day, dayFoods: dayFoods, dayPhysicalActivities: dayActivities };
+            setCompleteDay(completeDayData);
+
+            return completeDayData; 
+        } 
+        catch (error) {
             console.log(error);
+        }
+    };
+
+    const updateGrams = async (foodName, grams) => {
+        setUpdateGramsError(null);
+        if (grams <= 0) {
+            setUpdateGramsError('Grams must be greater than 0');
+            return;
+        }
+        try {
+            const formattedDate = formatDate(date);
+            const response = await axios.put(`https://localhost:7094/api/Days/change-grams`, {
+                date: formattedDate,
+                foodName: foodName,
+                grams: grams
+            }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            getCompleteDay(date).then((day) => updateGramsState(day));
+            return response.data;
+        }
+        catch (error) {
+            console.log(error);
+            setUpdateGramsError(error.response.data);
+        }
+    };
+
+    const updateMinutes = async (activityName, minutes) => {
+        setUpdateMinutesError(null);
+        if (minutes <= 0) { 
+            setUpdateMinutesError('Minutes must be greater than 0');
+            return;
+        }
+        try {
+            const formattedDate = formatDate(date);
+            const response = await axios.put(`https://localhost:7094/api/Days/change-minutes`, {
+                date: formattedDate,
+                activityName: activityName,
+                minutes: minutes
+            }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            getCompleteDay(date).then((day) => updateMinutesState(day));
+            return response.data;
+        }
+        catch (error) {
+            console.log(error);
+            setUpdateMinutesError(error.response.data);
         }
     };
 
@@ -73,10 +133,9 @@ function Day() {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             });
-            console.log(response);
             getCompleteDay(date);
         }
-        catch(error) {
+        catch (error) {
             console.log(error);
         }
     };
@@ -89,17 +148,44 @@ function Day() {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             });
-            console.log(response);
             getCompleteDay(date);
         }
-        catch(error) {
+        catch (error) {
             console.log(error);
         }
     };
-    
+
+    const updateGramsState = (day) => {
+        const initialGrams = {};
+        day.dayFoods.forEach(food => {
+            initialGrams[food.details.name] = food.grams;
+        });
+        setGrams(initialGrams);
+    };
+
+    const updateMinutesState = (day) => {
+        const initialMinutes = {};
+        day.dayPhysicalActivities.forEach(activity => {
+            initialMinutes[activity.details.name] = activity.minutes;
+        });
+        setMinutes(initialMinutes);
+    };
+
+    // Runs when the date changes (when the user clicks on the previous or next day buttons)
     useEffect(() => {
         if (date) {
-            getCompleteDay(date);
+            getCompleteDay(date)
+                .then((day) => {
+                    if (day && day.dayFoods) {
+                        updateGramsState(day);
+                    }
+                    if (day && day.dayPhysicalActivities) {
+                        updateMinutesState(day);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error fetching day:', error);
+                });
         }
     }, [date]);
 
@@ -116,7 +202,15 @@ function Day() {
                         return (
                             <li key={index}>
                                 <h3>{food.details.name}</h3>
-                                <p>Grams: {food.grams}</p>
+                                {!isCurrentDate && <p>Grams: {food.grams}</p>}
+                                {isCurrentDate && <p>Grams: 
+                                    <input 
+                                        type="number" 
+                                        value={grams[food.details.name]} 
+                                        onChange={(e) => updateGrams(food.details.name, e.target.value)}
+                                    />
+                                </p>}
+                                {updateGramsError && <p>{updateGramsError}</p>}
                                 <p>Calories: TO DO CALCULATE CALORIES</p>
                                 {isCurrentDate && <button onClick={() => {deleteFood(date, food.details.name)}}>Delete Food</button>}
                             </li>
@@ -131,7 +225,14 @@ function Day() {
                         return (
                             <li key={index}>
                                 <h3>{activity.details.name}</h3>
-                                <p>Minutes: {activity.minutes}</p>
+                                {!isCurrentDate  && <p>Minutes: {activity.minutes}</p>}
+                                {isCurrentDate && <p>Minutes:
+                                    <input 
+                                        type="number" 
+                                        value={minutes[activity.details.name]} 
+                                        onChange={(e) => updateMinutes(activity.details.name, e.target.value)}
+                                    />
+                                </p>}
                                 <p>Calories burned: TO DO CALCULATE CALORIES BURNED</p>
                                 {isCurrentDate && <button onClick={() => {deleteActivity(date, activity.details.name)}}>Delete Activity</button>}
                             </li>
