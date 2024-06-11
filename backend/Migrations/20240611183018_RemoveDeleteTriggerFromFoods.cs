85577f276a17bac7642f0705e322b0a36cc80686
ana <anamaria.hodivoianu@gmail.com>
2024-06-11 21:38:05 +0300

    fixed error deleting user who has foods

---
+++ backend/Migrations/20240611183018_RemoveDeleteTriggerFromFoods.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace healthy_lifestyle_web_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDeleteTriggerFromFoods : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            DROP TRIGGER IF EXISTS trg_DeleteFoods;
+        ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            -- Re-create the trigger if needed
+            CREATE TRIGGER trg_DeleteFoods
+            ON [dbo].[AspNetUsers]
+            FOR DELETE
+            AS
+            BEGIN
+                DELETE FROM [dbo].[Foods]
+                WHERE [ApplicationUserId] IN (SELECT [Id] FROM DELETED);
+            END
+        ");
+        }
+    }
+
+}
