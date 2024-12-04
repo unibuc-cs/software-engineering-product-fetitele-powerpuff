e482463edd573ba970502ee2e3549ab8ed823e12
Robu Corina <corinaflorentina28@gmail.com>
2024-12-04 09:29:33 +0200

    Adăugat endpoint-uri pentru adăugare și ștergere articole cu autorizare Admin

---
+++ backend/Repositories/ArticleRepository.cs
@@ -0,0 +1,54 @@
+﻿using healthy_lifestyle_web_app.Entities;
+using healthy_lifestyle_web_app.ContextModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace healthy_lifestyle_web_app.Repositories
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public ArticleRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddArticleAsync(Article article)
+        {
+            try
+            {
+                _context.Articles.Add(article);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false; 
+            }
+        }
+
+
+        public async Task<Article> GetByTitleAsync(string title)
+        {
+            return await _context.Articles.FirstOrDefaultAsync(a => a.Title == title);
+        }
+
+       
+
+        public async Task<bool> DeleteArticleAsync(Article article)
+        {
+            try
+            {
+                _context.Articles.Remove(article);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ backend/Repositories/IArticleRepository.cs
@@ -0,0 +1,12 @@
+﻿using healthy_lifestyle_web_app.Entities;
+
+namespace healthy_lifestyle_web_app.Repositories
+{
+    public interface IArticleRepository
+    {
+        public Task<bool> AddArticleAsync(Article article);
+        public Task<Article> GetByTitleAsync(string title);
+
+        public Task<bool> DeleteArticleAsync(Article article); 
+    }
+}
